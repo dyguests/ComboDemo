115e22947782ed88fb7125c6135c86d401e3ce43
dyguests <dyguests@qq.com>
2024-08-13 08:39:01 +0800

    animator override controller

---
+++ Assets/Scripts/AttackSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "Attack", menuName = "Attacks/Normal Attack", order = 0)]
+    public class AttackSO : ScriptableObject
+    {
+        public AnimatorOverrideController animatorOV;
+        public float damage;
+    }
+}
