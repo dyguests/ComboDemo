64f3d3c9a2a4a7b91ae615ff6d490352b0e761d8
dyguests <dyguests@qq.com>
2024-08-13 21:37:12 +0800

    all works.

---
+++ Assets/Scripts/Scenes/Games/AutoCombo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Scenes.Games
+{
+    public class AutoCombo : MonoBehaviour
+    {
+        [SerializeField] private AttackSO attack;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var player = other.GetComponent<PlayerCombat>();
+            if (player != null)
+            {
+                player.combo.Add(attack);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
