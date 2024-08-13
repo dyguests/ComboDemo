8b778d6fbe927467ddae50d0e3a93cd1ca416f9a
dyguests <dyguests@qq.com>
2024-08-13 21:24:35 +0800

    combo works.

--- Assets/Scripts/Scenes/Games/PlayerCombat.cs
+++ Assets/Scripts/Scenes/Games/PlayerCombat.cs
@@ -10,23 +10,58 @@ namespace Scenes.Games
         float lastComboEnd;
         int comboCounter;
 
-        Animator anim;
+        public Animator anim;
+        [SerializeField] Weapon weapon;
+
+        private void Start()
+        {
+            // todo impl
+            // anim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Attack();
+            }
+
+            ExitAttack();
+        }
 
         void Attack()
         {
-            if (Time.time - lastComboEnd > 0.2f && comboCounter <= combo.Count)
+            if (Time.time - lastComboEnd > 0.2f && comboCounter < combo.Count)
             {
                 CancelInvoke(nameof(EndCombo));
                 if (Time.time - lastClickedTime > 0.2f)
                 {
                     anim.runtimeAnimatorController = combo[comboCounter].animatorOV;
                     anim.Play("Attack", 0, 0);
+                    weapon.damage = combo[comboCounter].damage;
+                    comboCounter++;
+                    lastClickedTime = Time.time;
+
+                    if (comboCounter >= combo.Count)
+                    {
+                        comboCounter = 0;
+                    }
                 }
             }
         }
 
-        void ExitAttack() { }
+        void ExitAttack()
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f && anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+            {
+                Invoke(nameof(EndCombo), 1f);
+            }
+        }
 
-        void EndCombo() { }
+        void EndCombo()
+        {
+            comboCounter = 0;
+            lastComboEnd = Time.time;
+        }
     }
 }
