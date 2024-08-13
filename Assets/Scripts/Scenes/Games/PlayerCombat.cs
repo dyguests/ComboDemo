81385cd5db23d41c010b9294afedf928c11808fc
dyguests <dyguests@qq.com>
2024-08-13 09:01:43 +0800

    PlayerCombat

---
+++ Assets/Scripts/Scenes/Games/PlayerCombat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scenes.Games
+{
+    public class PlayerCombat : MonoBehaviour
+    {
+        public List<AttackSO> combo;
+        float lastClickedTime;
+        float lastComboEnd;
+        int comboCounter;
+        
+        // todo video 09:40
+        
+        void Attack() { }
+
+        void ExitAttack() { }
+
+        void EndCombo() { }
+    }
+}
